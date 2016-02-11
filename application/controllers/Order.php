804e159ca55042f1e171958f1d88dff92d297518
Casper Co <haln22111@gmail.com>
2016-02-11 10:29:51 -0800

    Step4 Other Half
    10:29AM - Feb 11

--- application/controllers/Order.php
+++ application/controllers/Order.php
@@ -2,9 +2,9 @@
 
 /**
  * Order handler
- * 
+ *
  * Implement the different order handling usecases.
- * 
+ *
  * controllers/welcome.php
  *
  * ------------------------------------------------------------------------
@@ -20,14 +20,14 @@ class Order extends Application {
         //FIXME
 
         $order_num = $this->Orders->highest() + 1;
-        
+
         $neworder = $this->Orders->create();
         $neworder->num = $order_num;
         $neworder->date = date();
         $neworder->status = 'a';
         $neworder->total = 0;
         $this->Orders->add($neworder);
-        
+
         redirect('/order/display_menu/' . $order_num);
     }
 
@@ -39,7 +39,7 @@ class Order extends Application {
         $this->data['pagebody'] = 'show_menu';
         $this->data['order_num'] = $order_num;
         //FIXME
-        $this->data['title'] = 'Order #'.$order_num;
+        $this->data['title'] = 'Order #'.$order_num . ' (' . number_format($this->Orders->total($order_num),2) . ')';
         // Make the columns
         $this->data['meals'] = $this->make_column('m');
         $this->data['drinks'] = $this->make_column('d');
@@ -50,17 +50,17 @@ class Order extends Application {
 	// child loop - used for the columns in the menu display.
 	// this feature, formerly in CI2.2, was removed in CI3 because
 	// it presented a security vulnerability.
-	// 
+	//
 	// This means that we cannot reference order_num inside of any of the
 	// variable pair loops in our view, but must instead make sure
-	// that any such substitutions we wish make are injected into the 
+	// that any such substitutions we wish make are injected into the
 	// variable parameters
 	// Merge this fix into your origin/master for the lab!
 	$this->hokeyfix($this->data['meals'],$order_num);
 	$this->hokeyfix($this->data['drinks'],$order_num);
 	$this->hokeyfix($this->data['sweets'],$order_num);
 	// end of hokey patch
-	
+
         $this->render();
     }
 
@@ -69,7 +69,7 @@ class Order extends Application {
 	foreach($varpair as &$record)
 	    $record->order_num = $order;
     }
-    
+
     // make a menu ordering column
     function make_column($category) {
         //FIXME
@@ -78,7 +78,7 @@ class Order extends Application {
 
     // add an item to an order
     function add($order_num, $item) {
-        //FIXME
+        $this->Orders->add_item($order_num, $item);
         redirect('/order/display_menu/' . $order_num);
     }
 
